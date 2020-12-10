6df82d3053ac081f99dadb2e189defb8fa38ccc5
Seiya Kawamoto <kid960805@gmail.com>
2020-12-11 01:56:25 +0900

    add exercise0130.f90

---
+++ exercise0103.f90
@@ -0,0 +1,12 @@
+program exercise0103
+    implicit none
+    integer :: i = 1234
+    real :: a = 1.234
+    double precision :: d = 9.87654321d0 ! 倍精度実数型
+    complex :: c = (1.5, 2.345) ! 複素数型
+    character(len=4) :: s = "abcd"
+    integer :: m, n
+    integer :: nmax = 10
+    character*30 :: fn, kata
+    
+end program exercise0103

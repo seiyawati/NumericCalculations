9b76d2bf8987c774d3303b6e06b2e718d11eeb92
Seiya Kawamoto <kid960805@gmail.com>
2020-12-26 14:06:48 +0900

    add: subroutine

--- Fortran入門/sumall.f90
+++ Fortran入門/sumall.f90
@@ -1,22 +1,38 @@
-program sumall
+subroutine sum_val(n,value,total)
+
     implicit none
 
+    integer, intent(in) :: n ! inはメインプログラムから呼ぶ
+    real(8), intent(inout) :: value(n) !inoutは両方
+    real(8), intent(out) :: total ! outはsubroutineからメインプログラムに呼ぶ
     integer :: i
-    real(8) :: value(10),total
 
-    do i=1,10
+    do i=1,n
         value(i) = i
     enddo
 
-    ! total=0.d0
-    ! do i=1,10
-    !     total = total + value(i)
-    ! enddo
-
-    ! 上記の処理をsum関数でやってくれる
     total = sum( value )
 
+end subroutine sum_val
+
+!-------------------------------------------------------------------
+
+program sumall
+    implicit none
+
+    integer :: n
+    real(8),allocatable :: value(:) ! 配列の動的割り付け
+    real(8) :: total
+
+    read(*,*) n
+
+    allocate( value(n) )
+
+    call sum_val(n,value,total)
+
     print *, total
+
+    deallocate( value ) !領域の解放を明示している
 
 
 end program sumall

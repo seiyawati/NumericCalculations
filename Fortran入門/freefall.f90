424bf44a9765c03be1e2380b81f6b2f4801d0c50
Seiya Kawamoto <kid960805@gmail.com>
2020-12-26 12:30:26 +0900

    add: do loop

--- Fortran入門/freefall.f90
+++ Fortran入門/freefall.f90
@@ -6,11 +6,12 @@ program freefall
     real(8) :: t ! 時間
     real(8) :: v0 ! 初期速度
     real(8) :: v ! 速度
+    integer :: i
     
     g = 9.8d0
 
     open(11, file='input.dat', status='old')
-    read(11,*) t
+    ! read(11,*) t
     read(11,*) v0
     ! t = 1.0d0
     ! print *, 'Please input time [s]'
@@ -18,11 +19,17 @@ program freefall
     ! print *, 'Please input initial velocity [m/s]'
     ! read(*,*) v0 ! *.*はデフォルトの書式で標準入力から入力することを示す
 
-    z = (1.0d0/2.0d0)*g*t**2d0 + v0*t
-    v = g*t + v0
-
     open(10, file='output.dat', status='replace') ! ファイルの指定, 5番と6番は使わない statusにはold, new. replace
-    write(10,*) z,v
+
+    do i=1,10
+
+        t =i
+        z = (1.0d0/2.0d0)*g*t**2d0 + v0*t
+        v = g*t + v0
+        write(10,*) z,v
+
+    enddo
+
     close(10)
     ! print *, z,v
 

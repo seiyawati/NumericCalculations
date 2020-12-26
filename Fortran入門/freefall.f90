7116036723abae2fb74e1879af45d55405d547e5
Seiya Kawamoto <kid960805@gmail.com>
2020-12-26 12:19:41 +0900

    add: file read write

--- Fortran入門/freefall.f90
+++ Fortran入門/freefall.f90
@@ -8,15 +8,22 @@ program freefall
     real(8) :: v ! 速度
     
     g = 9.8d0
+
+    open(11, file='input.dat', status='old')
+    read(11,*) t
+    read(11,*) v0
     ! t = 1.0d0
-    print *, 'Please input time [s]'
-    read(*,*) t ! *.*はデフォルトの書式で標準入力から入力することを示す
-    print *, 'Please input initial velocity [m/s]'
-    read(*,*) v0 ! *.*はデフォルトの書式で標準入力から入力することを示す
+    ! print *, 'Please input time [s]'
+    ! read(*,*) t ! *.*はデフォルトの書式で標準入力から入力することを示す
+    ! print *, 'Please input initial velocity [m/s]'
+    ! read(*,*) v0 ! *.*はデフォルトの書式で標準入力から入力することを示す
 
     z = (1.0d0/2.0d0)*g*t**2d0 + v0*t
     v = g*t + v0
 
-    print *, z,v
+    open(10, file='output.dat', status='replace') ! ファイルの指定, 5番と6番は使わない statusにはold, new. replace
+    write(10,*) z,v
+    close(10)
+    ! print *, z,v
 
 end program freefall
